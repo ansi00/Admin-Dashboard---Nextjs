dd25012abb31b6e5497c795182a183bccbb8dc7d
Anas Abbas <anasabbasmalik88@gmail.com>
2025-01-10 11:45:40 +0500

    Designed dummy UI for Rightbar component

--- app/ui/dashboard/rightbar/rightbar.jsx
+++ app/ui/dashboard/rightbar/rightbar.jsx
@@ -1,5 +1,49 @@
 import React from "react";
+import styles from "./rightbar.module.css";
+import Image from "next/image";
+import { MdPlayCircleFilled, MdReadMore } from "react-icons/md";
 
 export default function Rightbar() {
-  return <div>Rightbar</div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.item}>
+        <div className={styles.bgContainer}>
+          <Image src="/astronaut.png" className={styles.bg} fill />
+        </div>
+        <div className={styles.texts}>
+          <span className={styles.notification}>🔥 Available Now</span>
+          <h3 className={styles.title}>
+            How to use the new version of admin dashboard?
+          </h3>
+          <span className={styles.subtitle}>Take 4 minutes to learn</span>
+          <p className={styles.desc}>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae magnam
+            esse quas at?
+          </p>
+          <button className={styles.button}>
+            <MdPlayCircleFilled />
+            Watch
+          </button>
+        </div>
+      </div>
+      <div className={styles.item}>
+        <div className={styles.texts}>
+          <span className={styles.notification}>🚀 Coming Soon</span>
+          <h3 className={styles.title}>
+            New server actions are available, partial pre-rendering is coming
+            up!
+          </h3>
+          <span className={styles.subtitle}>Boost your productivity</span>
+          <p className={styles.desc}>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae magnam
+            esse quas at?
+          </p>
+          <button className={styles.button}>
+            <MdReadMore />
+            Learn
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 }
