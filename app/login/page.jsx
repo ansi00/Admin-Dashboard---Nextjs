650f36ee0be47f80ef17bc2306cc6e169d1c7bdd
Anas Abbas <anasabbasmalik88@gmail.com>
2025-01-13 17:32:44 +0500

    Completed dummy UI for SingleProduct and Login pages

--- app/login/page.jsx
+++ app/login/page.jsx
@@ -1,5 +1,24 @@
-import React from "react";
+import styles from "@/app/ui/login/login.module.css";
 
 export default function LoginPage() {
-  return <div>Login</div>;
+  return (
+    <div className={styles.container}>
+      <form className={styles.form}>
+        <h1>Login</h1>
+        <input
+          type="text"
+          name="username"
+          id="username"
+          placeholder="username"
+        />
+        <input
+          type="password"
+          name="password"
+          id="password"
+          placeholder="password"
+        />
+        <button>Login</button>
+      </form>
+    </div>
+  );
 }
