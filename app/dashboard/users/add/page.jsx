b28f13b51c4eafcd28aa1a54f42ab89dcdaaa36d
Anas Abbas <anasabbasmalik88@gmail.com>
2025-01-13 15:16:07 +0500

    Completed add User page

---
+++ app/dashboard/users/add/page.jsx
@@ -0,0 +1,41 @@
+import styles from "@/app/ui/dashboard/users/addUsers/addUsers.module.css";
+
+export default function AddUsers() {
+  return (
+    <div className={styles.container}>
+      <form className={styles.form}>
+        <input type="text" placeholder="username" name="username" required />
+        <input type="email" placeholder="email" name="email" required />
+        <input
+          type="password"
+          placeholder="password"
+          name="password"
+          required
+        />
+        <input type="text" placeholder="phone" name="phone" required />
+        <select name="isAdmin" id="isAdmin">
+          <option value={false} selected>
+            Is Admin
+          </option>
+          <option value={true}>Yes</option>
+          <option value={false}>No</option>
+        </select>
+        <select name="isActive" id="isActive">
+          <option value={true} selected>
+            Is Active
+          </option>
+          <option value={true}>Yes</option>
+          <option value={false}>No</option>
+        </select>
+        <textarea
+          name="address"
+          id="address"
+          cols="30"
+          rows="16"
+          placeholder="Address"
+        ></textarea>
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+  );
+}
