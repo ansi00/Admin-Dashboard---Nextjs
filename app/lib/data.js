dbb090e428a0beb384ac007a294f50f216f0f0f1
Anas Abbas <anasabbasmalik88@gmail.com>
2025-01-15 14:55:27 +0500

    Implemented search functionality for users

--- app/lib/data.js
+++ app/lib/data.js
@@ -1,10 +1,11 @@
 import { User } from "./models";
 import { connectToDB } from "./utisl";
 
-export const fetchUsers = async () => {
+export const fetchUsers = async (q) => {
+  const regex = new RegExp(q, "i");
   try {
     await connectToDB();
-    const users = await User.find();
+    const users = await User.find({ username: { $regex: regex } });
     return users;
   } catch (error) {
     console.log(error);
