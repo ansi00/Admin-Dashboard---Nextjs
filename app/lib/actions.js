a40cc56edc426e550afe1da195c5bf6f6a7d7de2
Anas Abbas <anasabbasmalik88@gmail.com>
2025-01-16 12:26:38 +0500

    Implemented deleteUser and deleteProduct functionality using server actions

--- app/lib/actions.js
+++ app/lib/actions.js
@@ -52,3 +52,29 @@ export const addProduct = async (formData) => {
   revalidatePath("/dashboard/products");
   redirect("/dashboard/products");
 };
+
+export const deleteProduct = async (formData) => {
+  const { id } = Object.fromEntries(formData);
+  try {
+    connectToDB();
+    await Product.findByIdAndDelete(id);
+  } catch (error) {
+    console.log(error);
+    throw new Error("Failed to delete product");
+  }
+
+  revalidatePath("/dashboard/products");
+};
+
+export const deleteUser = async (formData) => {
+  const { id } = Object.fromEntries(formData);
+  try {
+    connectToDB();
+    await User.findByIdAndDelete(id);
+  } catch (error) {
+    console.log(error);
+    throw new Error("Failed to delete user");
+  }
+
+  revalidatePath("/dashboard/users");
+};
