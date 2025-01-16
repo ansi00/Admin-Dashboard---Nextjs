9b2b7398a1ace05f168379c6e73e02d2c827a33f
Anas Abbas <anasabbasmalik88@gmail.com>
2025-01-16 12:05:04 +0500

    Implemented addUser functionality using server actions

--- app/dashboard/users/add/page.jsx
+++ app/dashboard/users/add/page.jsx
@@ -1,9 +1,10 @@
+import { addUser } from "@/app/lib/actions";
 import styles from "@/app/ui/dashboard/users/addUsers/addUsers.module.css";
 
 export default function AddUsers() {
   return (
     <div className={styles.container}>
-      <form className={styles.form}>
+      <form action={addUser} className={styles.form}>
         <input type="text" placeholder="username" name="username" required />
         <input type="email" placeholder="email" name="email" required />
         <input
@@ -14,16 +15,12 @@ export default function AddUsers() {
         />
         <input type="text" placeholder="phone" name="phone" required />
         <select name="isAdmin" id="isAdmin">
-          <option value={false} selected>
-            Is Admin
-          </option>
+          <option value={false}>Is Admin</option>
           <option value={true}>Yes</option>
           <option value={false}>No</option>
         </select>
         <select name="isActive" id="isActive">
-          <option value={true} selected>
-            Is Active
-          </option>
+          <option value={true}>Is Active</option>
           <option value={true}>Yes</option>
           <option value={false}>No</option>
         </select>
